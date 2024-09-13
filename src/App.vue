4c890675c2ea1b3fdb3f5680adb2f88470d9e571
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-09-13 15:45:34 +0200

    project scaffolding

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  body{
+    height: 100vh;
+    background: url('./assets/erba.jpeg');
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+</style>
